21c8371fa435a174864141e1c3cbecb4b15d9b96
bit-shashank <shashankcs083@gmail.com>
2021-07-09 01:16:28 +0530

    Added Temporary python Backend Folder

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+# All python image processing code will go here as of now
+# Will figure our project structure later
+
+import cv2
+img=cv2.imread("../resources/static/uploads/Andaman.jpg")
+cv2.imshow(img)
